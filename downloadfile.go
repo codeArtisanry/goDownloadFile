92aa4e046ce6248d186423ead24fe69ac83148ad
Vatsal <chauhanvatsal55@gmail.com>
2022-07-21 16:17:19 +0530

    feat: add download file

---
+++ downloadfile.go
@@ -0,0 +1,35 @@
+package godownloadfile
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"os"
+)
+
+func DownloadFile(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
+	}
+	//Create a empty file
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//Write the bytes to the fiel
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
